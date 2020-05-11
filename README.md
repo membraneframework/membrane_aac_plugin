26ad135cfc515c2358e2e2eb62df8c5b81f3b706
hajto <jakub.hajto@swmansion.com>
2020-05-11 21:25:22 +0200

    Added repo scaffold.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Membrane Multimedia Framework: AAC plugin
+
+[![CircleCI](https://circleci.com/gh/membraneframework/membrane_aac.svg?style=svg)](https://circleci.com/gh/membraneframework/membrane_aac)
+
+This package provides elements complimentary elements for AAC.
+
+It is a part of [Membrane Multimedia Framework](https://membraneframework.org).
+
+## Installation
+
+The package can be installed by adding `membrane_aac` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:membrane_aac, "~> 0.1.0"}
+  ]
+end
+```
+
+## Usage example
+
+TODO
+
+The docs can be found at [https://hexdocs.pm/membrane_aac](https://hexdocs.pm/membrane_aac).
+
+## Copyright and License
+
+Copyright 2020, [Software Mansion](https://swmansion.com/?utm_source=git&utm_medium=readme&utm_campaign=membrane_aac)
+
+[![Software Mansion](https://membraneframework.github.io/static/logo/swm_logo_readme.png)](https://swmansion.com/?utm_source=git&utm_medium=readme&utm_campaign=membrane_aac)
+
+Licensed under the [Apache License, Version 2.0](LICENSE)
---
+++ mix.exs
@@ -0,0 +1,74 @@
+defmodule Membrane.Element.AAC.MixProject do
+  use Mix.Project
+
+  @version "0.4.0"
+  @github_url "https://github.com/membraneframework/membrane_aac"
+
+  def project do
+    [
+      app: :membrane_aac_plugin,
+      version: @version,
+      elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps(),
+
+      # hex
+      description: "Membrane Multimedia Framework plugin for AAC",
+      package: package(),
+
+      # docs
+      name: "Membrane AAC plugin",
+      source_url: @github_url,
+      homepage_url: "https://membraneframework.org",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: docs()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: []
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github_url,
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      }
+    ]
+  end
+
+  defp deps do
+    [
+      {:membrane_core, "~> 0.5.1"},
+      {:bunch, "~> 1.0"},
+      {:membrane_caps_audio_aac, github: "membraneframework/membrane-caps-audio-aac"},
+
+      # Dev
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:excoveralls, ">= 0.8.0", only: :test}
+    ]
+  end
+end
