34b5ac4e23caae7069a5f5410547775036f75d49
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2021-12-07 12:55:20 +0100

    Bump version to 0.11

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.AAC.MixProject do
   use Mix.Project
 
-  @version "0.10.0"
+  @version "0.11.0"
   @github_url "https://github.com/membraneframework/membrane_aac_plugin"
 
   def project do
