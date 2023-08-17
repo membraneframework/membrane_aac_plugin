06c0a66ec78033c30d3e575100a32533d8ff2423
Noarkhh <jakub.pryc@gmail.com>
2023-08-17 15:58:34 +0200

    Add config conversion tests

--- test/membrane/aac/parser_test.exs
+++ test/membrane/aac/parser_test.exs
@@ -1,8 +1,10 @@
 defmodule Membrane.AAC.ParserTest do
   use ExUnit.Case
+  import Membrane.ChildrenSpec
   import Membrane.Testing.Assertions
+  alias Membrane.Testing.Pipeline
   alias Membrane.AAC.Parser
-  alias Membrane.Testing
+  alias Membrane.{AAC, Testing}
 
   @expected_timestamps [
     0,
@@ -13,9 +15,45 @@ defmodule Membrane.AAC.ParserTest do
     Ratio.new(51_200_000_000, 441)
   ]
 
-  test "integration" do
-    import Membrane.ChildrenSpec
+  defp perform_conversion_test(comparison_config, transition_config) do
+    fixture_pipeline_structure =
+      child(:file, %Membrane.File.Source{location: "test/fixtures/sample.aac"})
+      |> child(:parser, %Parser{output_config: comparison_config})
+      |> child(:sink, Testing.Sink)
 
+    conversion_pipeline_structure =
+      child(:file, %Membrane.File.Source{location: "test/fixtures/sample.aac"})
+      |> child(:parser1, %Parser{output_config: transition_config})
+      |> child(:parser2, %Parser{output_config: comparison_config})
+      |> child(:sink, Testing.Sink)
+
+    fixture_pipeline_pid =
+      Pipeline.start_link_supervised!(structure: fixture_pipeline_structure)
+
+    conversion_pipeline_pid =
+      Pipeline.start_link_supervised!(structure: conversion_pipeline_structure)
+
+    assert_pipeline_play(fixture_pipeline_pid)
+
+    assert_sink_stream_format(fixture_pipeline_pid, :sink, %AAC{
+      config: {^comparison_config, fixture_config}
+    })
+
+    assert_end_of_stream(fixture_pipeline_pid, :sink)
+
+    assert_pipeline_play(conversion_pipeline_pid)
+
+    assert_sink_stream_format(conversion_pipeline_pid, :sink, %AAC{
+      config: {^comparison_config, ^fixture_config}
+    })
+
+    assert_end_of_stream(conversion_pipeline_pid, :sink)
+
+    Pipeline.terminate(fixture_pipeline_pid)
+    Pipeline.terminate(conversion_pipeline_pid)
+  end
+
+  test "integration" do
     pipeline =
       Testing.Pipeline.start_link_supervised!(
         structure:
@@ -58,14 +96,14 @@ defmodule Membrane.AAC.ParserTest do
     Testing.Pipeline.terminate(pipeline, blocking?: true)
   end
 
-  test "correct AAC stream format is generated in response to Membrane.AAC.RemoteStream format" do
+  test "correct AAC stream format is generated in response to provided audio specific config" do
     {_actions, state} =
       Parser.handle_init(nil, %Parser{
         out_encapsulation: :none,
         in_encapsulation: :none
       })
 
-    input_stream_format = %Membrane.AAC{
+    input_stream_format = %AAC{
       config:
         {:audio_specific_config,
          <<
@@ -88,7 +126,12 @@ defmodule Membrane.AAC.ParserTest do
     assert {[stream_format: {:output, output_stream_format}], _state} =
              Parser.handle_stream_format(:input, input_stream_format, nil, state)
 
-    assert %Membrane.AAC{profile: :LC, sample_rate: 44_100, channels: 2, samples_per_frame: 960} =
+    assert %AAC{profile: :LC, sample_rate: 44_100, channels: 2, samples_per_frame: 960} =
              output_stream_format
+  end
+
+  test "the output stream format is the same as input when converting between configs" do
+    perform_conversion_test(:esds, :audio_specific_config)
+    perform_conversion_test(:audio_specific_config, :esds)
   end
 end
