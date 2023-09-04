c462adc52352ccc34c54ff2eccf7bd642ef04418
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2023-09-04 13:06:25 +0200

    Fix parsing esds (#30)
    * Fix parsing section 5 of esds
    
    * Remove code duplication of parsing and generating AudioSpecificConfig
    
    Co-authored-by: Bartosz Błaszków <bbartosz06@gmail.com>

--- lib/membrane/aac/parser/config.ex
+++ lib/membrane/aac/parser/config.ex
@@ -1,6 +1,7 @@
 defmodule Membrane.AAC.Parser.Config do
   @moduledoc false
 
+  require Membrane.Logger
   alias Membrane.AAC
   alias Membrane.AAC.Parser.{AudioSpecificConfig, Esds}
 
@@ -23,10 +24,25 @@ defmodule Membrane.AAC.Parser.Config do
   def parse_config(stream_format) do
     case stream_format.config do
       {:esds, esds} ->
-        Map.merge(stream_format, Esds.parse_esds(esds))
+        esds_stream_format = Esds.parse_esds(esds)
 
-      {:audio_specific_config, config} ->
-        AudioSpecificConfig.parse_audio_specific_config(config)
+        [:sample_rate, :channels]
+        |> Enum.map(fn field ->
+          {field, Map.fetch!(esds_stream_format, field), Map.fetch!(stream_format, field)}
+        end)
+        |> Enum.filter(fn {_field, esds_field, stream_field} -> esds_field != stream_field end)
+        |> Enum.each(fn {field, esds_field, stream_field} ->
+          Membrane.Logger.warning("""
+          #{inspect(field)} decoded from esds differs from stream_format:
+          esds: #{inspect(esds_field)}
+          stream_format: #{inspect(stream_field)}
+          """)
+        end)
+
+        esds_stream_format
+
+      {:audio_specific_config, audio_specific_config} ->
+        AudioSpecificConfig.parse_audio_specific_config(audio_specific_config)
 
       nil ->
         stream_format
