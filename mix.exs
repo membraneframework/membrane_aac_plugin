b9fcf1b4b9b6a34de08d7e6768d68fbc79937926
noarkhh <jakub.pryc@gmail.com>
2025-03-05 17:04:51 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.AAC.MixProject do
   use Mix.Project
 
-  @version "0.19.0"
+  @version "0.19.1"
   @github_url "https://github.com/membraneframework/membrane_aac_plugin"
 
   def project do
